a0adad116ce9ea8a8ac7bf2622f1e561244f8a1f
Colin Reisterer <creisterer@projectdecibel.com>
2019-01-07 20:22:49 -0500

    Result of Xcode New Project

---
+++ UnitTestWithoutApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTestWithoutApp
+//
+//  Created by Colin Reisterer on 1/7/19.
+//  Copyright © 2019 FullyTyped. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
